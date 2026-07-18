09cc2f33d8a5e5a53687cb6809d26534f5e96852
ayush220226 <scrazyhub@gmail.com>
2026-07-18 07:54:03 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
-// AYUKSHA SCRIPT
+const searchBox = document.getElementById("searchBox");
+
+if(searchBox){
+
+searchBox.addEventListener("keyup", function(){
+
+let value = this.value.toLowerCase();
+
+let cards = document.querySelectorAll(".app-card");
+
+cards.forEach(function(card){
+
+let text = card.innerText.toLowerCase();
+
+if(text.indexOf(value) > -1){
+
+card.style.display="flex";
+
+}else{
+
+card.style.display="none";
+
+}
+
+});
+
+});
+
+}
